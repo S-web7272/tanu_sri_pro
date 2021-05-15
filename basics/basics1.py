aca6d17aadee355635618e8688aaa219234151f3
smita srivastava <tanusrivastava7272@gmail.com>
2021-05-15 21:23:25 +0530

    first commit

---
+++ basics/basics1.py
@@ -0,0 +1,4 @@
+print("hello , my name is smita srivastava")
+print("I am a software engineer and good ptrogrammer")
+print('I am a student')
+print('my hobby is a reading book, listening music')
---
+++ basics/condition1.py
@@ -0,0 +1,19 @@
+a = 10
+if a > 5:
+    print('this is a grater than 5')
+
+if a>15:
+    print('this is a also bigger that 15')
+
+
+if a == 10:
+    print('this is equal to 10')
+
+if a==5 or a<15:
+    print('this condition is true')
+    print('any one of them is true')
+    
+
+
+
+
