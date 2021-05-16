0e924d6b26d4b821ed2981ca6237805299c155c1
smita srivastava <tanusrivastava7272@gmail.com>
2021-05-16 14:27:55 +0530

    add commit

---
+++ list_in_python/creating_list.py
@@ -0,0 +1,25 @@
+numbers = [1,2,3,4,5,6]
+names = ['ravi','shiva','eshita','neha','ashi','ajay']
+
+heights = ['ravi',5.6,'shiva',6,'eshita',5.1]
+info = [True,True,False,True,False]
+
+marks_per_year = [[45,67,45],[67,34,26],[78,56,34]]
+teams =[['raja','rani','raju'],['jack','jamie'],['alex','alan']]
+smileys = ['@','#','*']
+
+print(numbers,'size= ',len(numbers))
+print(names,'size= ',len(names))
+print(heights,'size= ',len(heights))
+print(marks_per_year,'size= ',len(marks_per_year))
+print(info,'size= ',len(info))
+print(teams,'size= ',len(teams))
+print(smileys)
+
+#generating a large list
+one_crore_items = list(range(1,100001))
+even_list = list(range(2,201,2))
+print(even_list)
+
+
+
