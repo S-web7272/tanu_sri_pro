0cce385916b062ce8f235d70f677cef02b0ec1d9
smita srivastava <tanusrivastava7272@gmail.com>
2021-05-22 15:50:47 +0530

    add commit

--- list_practice/question_3.py
+++ list_practice/question_3.py
@@ -1,2 +1,10 @@
 # wap to create a nested list using user input,the length of list depens  upon the user:
-    
+
+l = int(input('enter the lenght of nested list'))
+nested = []
+for i in range(l):
+    nested.append([])
+    k = int(input('enter the list {}'.format(i)))
+    for j in range(k):
+        nested[i].append(j)
+print(nested)
---
+++ list_practice/question_5.py
@@ -0,0 +1,2 @@
+# wap to generate a list of cricketers entered by user  only if the first letter of the name is capital.the size will depend upon user:
+
